e24bae32ae21a4275cc212875a57a4d0398ef3f0
saicharan20012007 <saicharan07071979@gmail.com>
2024-01-03 23:31:10 +0000

    first commit

---
+++ src/components/CountriesVisiting/index.js
@@ -0,0 +1,149 @@
+import './index.css'
+import {Component} from 'react'
+
+const initialCountriesList = [
+  {
+    id: '53c9c67a-c923-4927-8a75-fdfc4bc5ec61',
+    name: 'Australia',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-australia-img.png',
+    isVisited: false,
+  },
+  {
+    id: '8baa8029-fb2c-4f06-bfcc-3dc9ad12b24d',
+    name: 'Canada',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-canada-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1b520f98-6548-41f3-816e-c8b887865172',
+    name: 'Greenland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-greenland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '25841996-fbfd-4554-add4-4c94082c8ccd',
+    name: 'India',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-india-img.png',
+    isVisited: true,
+  },
+  {
+    id: '603c3568-13b0-11ec-82a8-0242ac130003',
+    name: 'Netherlands',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-netherland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '3c988dec-55e1-477d-a9e2-b354fd559849',
+    name: 'Portugal',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-portugal-img.png',
+    isVisited: false,
+  },
+  {
+    id: 'd766f754-34f7-413e-81ec-9992821b97fa',
+    name: 'Switzerland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-switzerland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '7ebb4e04-b124-417f-a69e-564a456d70f1',
+    name: 'Thailand',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-thailand-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1e4b1dcd-6ace-4dde-ad8d-675927d5ae47',
+    name: 'United Kingdom',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-united-kingdom-img.png',
+    isVisited: true,
+  },
+  {
+    id: 'e76da8ca-bc48-4981-902b-a4d2d46feb6d',
+    name: 'Venezuela',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-venezuela-img.png',
+    isVisited: false,
+  },
+]
+
+class CountriesVisiting extends Component {
+  state = {places: initialCountriesList}
+
+  visitThisCountry = id => {
+    this.setState(prevState => ({
+      places: prevState.places.map(each => {
+        if (each.id === id) {
+          return {...each, isVisited: !each.isVisited}
+        }
+        return each
+      }),
+    }))
+  }
+
+  render() {
+    const {places} = this.state
+    const visitedPlaces = places.filter(each => each.isVisited === true)
+    console.log(places)
+    return (
+      <div className="bg-container">
+        <h1 className="title">Countries</h1>
+        <ul className="sub-container">
+          {places.map(each => (
+            <li className="country-item" key={each.id}>
+              <p className="country-name">{each.name}</p>
+              {each.isVisited ? (
+                <p className="visited-text">Visited</p>
+              ) : (
+                <button
+                  type="button"
+                  className="visit-btn"
+                  onClick={() => this.visitThisCountry(each.id)}
+                >
+                  Visit
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <h1 className="title">Visited Countries</h1>
+        <ul className="visited-countries-container">
+          {visitedPlaces.length === 0 ? (
+            <div className="no-countries-container">
+              <p className="country-name">No Countries Visited Yet</p>
+            </div>
+          ) : (
+            visitedPlaces.map(each => (
+              <li className="visited-item" id={each.id}>
+                <img
+                  src={each.imageUrl}
+                  alt="thumbnail"
+                  className="flag-image"
+                />
+                <div className="sub-item-sub-container">
+                  <p className="country-name">{each.name}</p>
+                  <button
+                    className="remove-btn"
+                    onClick={() => this.visitThisCountry(each.id)}
+                    type="button"
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default CountriesVisiting
